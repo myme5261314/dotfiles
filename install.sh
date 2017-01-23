4c61d0251484adbdefed4c1d0a529f4408b9c607
Peng Liu <myme5261314@gmail.com>
2017-01-23 10:21:25 +0800

    Update on ubuntu.

--- install.sh
+++ install.sh
@@ -20,9 +20,58 @@ elif [[ "$unamestr" == "Darwin" ]]; then
 fi
 
 # Pre dependencies install.
-if [[ "$platform" == "Linux" ]];then
-    # Useless.
-    sudo apt-get install source-code-pro
+if [[ "$platform" == "linux" ]];then
+    sudo apt-get update
+    # Docker
+    sudo apt-get install apt-transport-https ca-certificates
+    sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+    sudo echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+    sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+    # Apt-fast
+    sudo add-apt-repository -y ppa:saiarcot895/myppa
+    # Fasd
+    sudo add-apt-repository -y ppa:aacebedo/fasd
+    # Dropbox
+    echo "deb http://linux.dropbox.com/ubuntu xenial main"|sudo tee /etc/apt/sources.list.d/dropbox.list
+    sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
+    # Apt proxy
+    sudo ln -sf $root/apt/01proxy /etc/apt/apt.conf.d/01proxy
+    sudo apt-get update
+    # Apt-fast
+    sudo apt-get -y install apt-fast
+    sudo apt-fast install -y aria2
+    # Fasd
+    sudo apt-fast install -y fasd
+    # Docker
+    sudo apt-fast install -y docker-engine
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo echo "DOCKER_OPTS=\"\$DOCKER_OPTS --registry-mirror=http://hub-mirror.c.163.com\"" >> /etc/default/docker
+    cat /etc/default/docker | uniq | sudo tee /etc/default/docker
+    # Source Code Pro.
+    [ -d /usr/share/fonts/opentype ] || sudo mkdir /usr/share/fonts/opentype && sudo git clone --depth 1 --branch release https://github.com/adobe-fonts/source-code-pro.git /usr/share/fonts/opentype/scp && sudo fc-cache -f -v &
+    sudo apt-fast install -y ctags
+    sudo apt-fast install -y vim-gnome emacs
+    sudo apt-fast install -y privoxy
+    sudo apt-fast install -y proxychains
+    mkdir ~/.proxychains
+    ln -sf $root/proxychains.conf ~/.proxychians/proxychains.conf
+    # Dropbox
+    sudo apt-get install -y dropbox
+    cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+    dropbox start
+    ln -sf ~/Dropbox/org ~/org-notes
+    sudo apt-fast install -y nodejs npm
+    npm install -g cnpm --registry=https://registry.npm.taobao.org
+    sudo apt-fast install -y pandoc
+    sudo pip install jedi
+    sudo apt-fast install -y supervisor
+    sudo rm -rf /etc/supervisor/conf.d && sudo ln -sf $root/supervisor /etc/supervisor/conf.d
+    ln -sf $root/aria2 ~/.aria2
+    pkill -f "aria2c" && aria2c -D --conf-path=~/.aria2/aria2-chrome.conf
+    mkdir ~/.config && ln -sf $root/autostart_ubuntu ~/.config/autostart
+    # yapf
+    sudo pip install yapf
 elif [[ "$platform" == "osx" ]];then
     # Emacs.
     brew tap caskroom/fonts
@@ -62,6 +111,8 @@ elif [[ "$platform" == "osx" ]];then
     # Virtualbox
     brew cask install virtualbox
 
+    # yapf
+    pip install yapf
 fi
 
 # Checkout to OS branch.
@@ -74,7 +125,7 @@ git submodule update
 echo "Install vim configurations..."
 
 echo "clear .vim directory"
-rm -rf ~/.vim
+#rm -rf ~/.vim
 echo "Install Vundle for vim."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
@@ -84,17 +135,19 @@ rm ~/.vimrc
 rm ~/.gvimrc
 ln -s $root/vim/.vimrc ~/.vimrc
 ln -s $root/vim/.vimrc ~/.gvimrc
+nohup vim +BundleUpdate &
 
 echo "Install Spacemacs configurations..."
-mv -f ~/.emacs.d ~/.emacs.d.old
-mv -f ~/.spacemacs.d ~/.spacemacs.d.old
+#mv -f ~/.emacs.d ~/.emacs.d.old
+#mv -f ~/.spacemacs.d ~/.spacemacs.d.old
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 cd ~/.emacs.d && git checkout develop && cd $root
 ln -s $root/spacemacs ~/.spacemacs.d
 
 echo "Install zshrc configurations..."
 if [[ "$platform" == "linux" ]]; then
-    sudo apt-get install zsh-antigen
+    sudo apt-fast install -y global
+    sudo apt-fast install -y zsh-antigen
 elif [[ "$platform" == "osx" ]]; then
     brew install antigen
 fi
@@ -103,9 +156,9 @@ ln -s $root/zshrc/.zshrc ~/.zshrc
 
 echo "Install TheFuck..."
 if [[ "$platform" == "linux" ]]; then
-    sudo apt-get install zsh-antigen
-    sudo apt update
-    sudo apt install python3-dev python3-pip
+    sudo apt-fast install -y zsh-antigen
+    chsh -s /usr/bin/zsh
+    sudo apt-fast install -y python3-dev python3-pip
     sudo -H pip3 install thefuck
 elif [[ "$platform" == "osx" ]]; then
     brew install thefuck
@@ -113,4 +166,4 @@ fi
 
 # Pip configuration.
 mkdir ~/.pip
-ln -s $root/pip.conf ~/.pip/pip.conf
+ln -sf $root/pip.conf ~/.pip/pip.conf
